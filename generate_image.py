e04f27413c7989ce567cb23033b3b7b8962fb0a6
yuvalmiz <yuvalmizrahi@mail.tau.ac.il>
2024-09-24 05:59:10 -0500

    ..

--- generate_image.py
+++ generate_image.py
@@ -1,14 +1,8 @@
 from diffusers import StableDiffusionPipeline, AutoencoderKL
-import torch
-from api import read_csv_to_list, log_to_csv
-import os
-import gc
-from tqdm import tqdm
-import random
 
 
 trained_model_path = "stableDiffusion/output_model_full_longer"
-vae = AutoencoderKL.from_pretrained('stableDiffusion/vaeFineTuned/vae_checkpoint_epoch_1')
+vae = AutoencoderKL.from_pretrained('stableDiffusion/vaeFineTuned/vae_checkpoint_epoch_1', torch_dtype=torch.float16)
 pipe = StableDiffusionPipeline.from_pretrained(trained_model_path, torch_dtype=torch.float16, vae=vae)
 
 prompt = "a game world map"
