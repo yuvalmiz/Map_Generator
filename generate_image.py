b5f40b83e078639909eeeb842f4d0b5c6dd706fc
yuvalmiz <92447017+yuvalmiz@users.noreply.github.com>
2024-09-26 18:11:54 +0300

    Update generate_image.py

--- generate_image.py
+++ generate_image.py
@@ -1,10 +1,10 @@
 from diffusers import StableDiffusionPipeline, AutoencoderKL
 
 
-trained_model_path = "stableDiffusion/output_model_full_longer"
-vae = AutoencoderKL.from_pretrained('stableDiffusion/vaeFineTuned/vae_checkpoint_epoch_1', torch_dtype=torch.float16)
+trained_model_path = "stableDiffusion/output_model"
+vae = AutoencoderKL.from_pretrained('vaeFineTuned/vae_checkpoint_epoch_1', torch_dtype=torch.float16) # change to your favorite VAE
 pipe = StableDiffusionPipeline.from_pretrained(trained_model_path, torch_dtype=torch.float16, vae=vae)
 
-prompt = "a game world map"
+prompt = "a game world map" # change prompt to what you want to generate
 
 pipe(prompt).images[0].save("test_image.png")
